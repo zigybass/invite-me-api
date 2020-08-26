ab0a7e59d1eda2dc3c5ab4af1764f57988cfe1db
Zigy <zigybass@gmail.com>
2020-08-26 18:05:01 -0400

    initialized java spring api

---
+++ invite-me-api/src/test/java/com/inviteme/api/InviteMeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inviteme.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InviteMeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
